02bb72960eae2e422be802cf5327300eb359d168
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-02-11 16:45:11 -0300

    Error in formatting date handled

--- github-user-search/src/core/components/SearchInfo/index.tsx
+++ github-user-search/src/core/components/SearchInfo/index.tsx
@@ -7,8 +7,7 @@ type Params = {
 }
 
 const SearchInfo = ({ responseData }: Params) => {
-    const data = dayjs(responseData.created_at?.substr(0, 10).replaceAll('-', '/')).format('DD/MM/YYYY')
-
+   
     return (
         <main>
             <div className="user-info-numbers">
@@ -24,16 +23,16 @@ const SearchInfo = ({ responseData }: Params) => {
             <div className="user-details-card data-response">
                 <h3 className="user-info-title">Informações </h3>
                 <div>
-                <p className="user-info-content"><strong>Empresa: </strong>{responseData?.company}</p>
+                <p className="user-info-content"><strong>Empresa: </strong>{responseData.company}</p>
                 </div>
                 <div>
-                <p className="user-info-content"><strong>Website/Blog: </strong>{responseData?.blog === "" ? "Nenhum blog/website informado" : responseData?.blog}</p>
+                <p className="user-info-content"><strong>Website/Blog: </strong>{responseData.blog === "" ? "Nenhum blog/website informado" : responseData?.blog}</p>
                 </div>
                 <div>
-                <p className="user-info-content"><strong>Localidade: </strong>{responseData?.location}</p>
+                <p className="user-info-content"><strong>Localidade: </strong>{responseData.location}</p>
                 </div>
                 <div>
-                <p className="user-info-content"><strong>Membro desde: </strong>{data}</p>
+                <p className="user-info-content"><strong>Membro desde: </strong>{responseData.created_at}</p>
                 </div>                
             </div>
         </main>
